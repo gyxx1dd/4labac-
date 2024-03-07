4d097c3b7b7185d944903fe48fe2a3e83605f8b5
gyxx1dd <skiv08099@gmail.com>
2024-03-07 14:41:41 +0200

    4laba#
    4labac#

---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3labac_
+{
+    public partial class Form1 : Form
+    {
+        private Wolf wolf;
+
+        public Form1()
+        {
+            InitializeComponent();
+            wolf = new Wolf(30, 3, 50, "Grey Wolf", "Forests of North America");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Wolf Information:\n\n" +
+                            $"Weight: {wolf.Weight} kg\n" +
+                            $"Age: {wolf.Age} years\n" +
+                            $"Daily Maintenance Cost: {wolf.DailyMaintenanceCost}$\n" +
+                            $"Breed: {wolf.Breed}\n" +
+                            $"Natural Habitat: {wolf.NaturalHabitat}",
+                            "Wolf Info",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information);
+        }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3labac_
+{
+    public interface IDisplayable
+    {
+        void DisplayInfo();
+    }
+
+    public interface IComparable<T>
+    {
+        int CompareTo(T other);
+    }
+
+    public interface ICloneable
+    {
+        object Clone();
+    }
+
+    public class Animal : IDisplayable, IComparable<Animal>, ICloneable
+    {
+        public double Weight { get; set; }
+        public int Age { get; set; }
+        public double DailyMaintenanceCost { get; set; }
+
+        public Animal(double weight, int age, double dailyMaintenanceCost)
+        {
+            Weight = weight;
+            Age = age;
+            DailyMaintenanceCost = dailyMaintenanceCost;
+        }
+
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"Weight: {Weight} kg");
+            Console.WriteLine($"Age: {Age} years");
+            Console.WriteLine($"Daily Maintenance Cost: {DailyMaintenanceCost}$");
+        }
+
+        public int CompareTo(Animal other)
+        {
+            if (other == null)
+                return 1;
+            return this.Weight.CompareTo(other.Weight);
+        }
+
+        public object Clone()
+        {
+            return new Animal(this.Weight, this.Age, this.DailyMaintenanceCost);
+        }
+    }
+
+    public class Wolf : Animal
+    {
+        public string Breed { get; set; }
+        public string NaturalHabitat { get; set; }
+
+        public Wolf(double weight, int age, double dailyMaintenanceCost, string breed, string naturalHabitat)
+            : base(weight, age, dailyMaintenanceCost)
+        {
+            Breed = breed;
+            NaturalHabitat = naturalHabitat;
+        }
+
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine($"Breed: {Breed}");
+            Console.WriteLine($"Natural Habitat: {NaturalHabitat}");
+        }
+
+        public int CompareTo(Wolf other)
+        {
+            if (other == null)
+                return 1;
+            return this.Weight.CompareTo(other.Weight);
+        }
+
+        public new object Clone()
+        {
+            return new Wolf(this.Weight, this.Age, this.DailyMaintenanceCost, this.Breed, this.NaturalHabitat);
+        }
+    }
+}
